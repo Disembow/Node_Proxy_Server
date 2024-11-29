c61ec7fee3fc68877d659871431c7b2b7c2f456d
Yauheni Naliotau <y.naliotau@godeltech.com>
2024-11-29 12:08:51 +0100

    task-5-6: create first endpoint

--- index.js
+++ index.js
@@ -6,15 +6,20 @@ const { BASE_API_URL, PORT, API_KEY, START_DATE, END_DATE } = config;
 
 const requestUrl = `${BASE_API_URL}/neo/rest/v1/feed?start_date=${START_DATE}&end_date=${END_DATE}&api_key=${API_KEY}`;
 
-await axios
-  .get(requestUrl)
-  .then((res) => console.log(JSON.stringify(res.data, null, 2)))
-  .catch((err) => console.error(err));
-
 const app = express();
 
 app.get("/", (_, res) => {
   res.send("Server works");
+});
+
+app.get("/meteors", async (_, res) => {
+  try {
+    const data = await axios.get(requestUrl);
+    res.status(200).json(data.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: error });
+  }
 });
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}...`));
