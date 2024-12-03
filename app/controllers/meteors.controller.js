86eaca749dca98a655ebb8df609d0b08536b719d
Yauhen Naliotau <106077599+Disembow@users.noreply.github.com>
2024-12-03 13:51:07 +0100

    Task-9: Add parameters to the query (#2)
    * task-9: add parameters to the query
    
    * task-9: update postman collection

--- app/controllers/meteors.controller.js
+++ app/controllers/meteors.controller.js
@@ -1,16 +1,10 @@
 import { getReducedMeteors } from "../services/meteors.service.js";
 
-const DEFAULT_START_DATE = "2024-11-17";
-const DEFAULT_END_DATE = "2024-11-18";
-
 export const getMeteors = async (req, res, next) => {
   try {
-    const { startDate, endDate } = req.query;
+    const { date, count, isDangerous } = req.query;
 
-    const response = await getReducedMeteors(
-      startDate || DEFAULT_START_DATE,
-      endDate || DEFAULT_END_DATE
-    );
+    const response = await getReducedMeteors(date, count, isDangerous);
 
     res.status(200).json(response);
   } catch (error) {
--- app/services/meteors.service.js
+++ app/services/meteors.service.js
@@ -1,9 +1,12 @@
 import { getMeteors } from "../repositories/meteors.repository.js";
-import { parseMeteors } from "../utils/parseMeteors.js";
+import { getCurrentWeekDates } from "../utils/parseDate.js";
+import { filterMeteors } from "../utils/parseMeteors.js";
 
-export const getReducedMeteors = async (startDate, endDate) => {
+export const getReducedMeteors = async (date, count, isDangerous) => {
+  const [startDate, endDate] = getCurrentWeekDates(date);
+
   const data = await getMeteors(startDate, endDate);
-  const parsedMeteors = parseMeteors(data);
+  const parsedMeteors = filterMeteors(data, count, isDangerous);
 
   return parsedMeteors;
 };
